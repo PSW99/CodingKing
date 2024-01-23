fb7ef9a4d02692c20f5cc17425c530ecdf0e0f9b
PSW99 <105043010+PSW99@users.noreply.github.com>
2024-01-23 16:06:16 +0900

    [Silver III] Title: 1로 만들기, Time: 236 ms, Memory: 57960 KB -BaekjoonHub

---
+++ 백준/Silver/1463. 1로 만들기/1로 만들기.java
@@ -0,0 +1,38 @@
+
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.LinkedList;
+import java.util.Queue;
+
+public class Main {
+    private static int N;
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        N = Integer.parseInt(br.readLine());
+        int[] visited = new int[N + 1];
+        visited[N] = 1;
+        Queue<Integer> queue = new LinkedList<>();
+        queue.add(N);
+         while (queue.isEmpty() == false) {
+            int n = queue.remove();
+             if (n == 1) {
+                 N = visited[n] - 1;
+             }
+             if (n - 1 >= 0 && visited[n - 1] == 0) {
+                 visited[n - 1] = visited[n] + 1;
+                 queue.add(n - 1);
+             }
+             if (n % 3 == 0 && n >= 3 && visited[n / 3] == 0) {
+                 visited[n / 3] = visited[n] + 1;
+                 queue.add(n / 3);
+             }
+             if (n % 2 == 0 && n >= 2 && visited[n / 2] == 0) {
+                 visited[n / 2] = visited[n] + 1;
+                 queue.add(n / 2);
+             }
+        }
+        System.out.println(N);
+    }
+}
