3c6352567b11a92f7ea714fac03ab06f7a12bf7a
PSW99 <105043010+PSW99@users.noreply.github.com>
2024-01-26 21:17:25 +0900

    [Silver III] Title: 2×n 타일링, Time: 148 ms, Memory: 14264 KB -BaekjoonHub

---
+++ 백준/Silver/11726. 2×n 타일링/2×n 타일링.java
@@ -0,0 +1,24 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+
+public class Main {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        int N = Integer.parseInt(br.readLine());
+        int[] dp = new int[N + 1];
+        if (N == 1) {
+            System.out.println(1);
+        } else if (N == 2) {
+            System.out.println(2);
+        } else {
+            dp[1] = 1;
+            dp[2] = 2;
+            for (int i = 3; i < dp.length; i++) {
+                dp[i] = ((dp[i-1] % 10007) + (dp[i-2]% 10007)) % 10007;
+            }
+            System.out.println(dp[N]);
+        }
+
+    }
+}
