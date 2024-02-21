26dbef1f0d8306849c0a802bea3c1d7721e3fb01
PSW99 <105043010+PSW99@users.noreply.github.com>
2024-02-21 16:51:11 +0900

    [Silver II] Title: 가희와 키워드, Time: 1148 ms, Memory: 222812 KB -BaekjoonHub

--- 백준/Silver/22233. 가희와 키워드/가희와 키워드.java
+++ 백준/Silver/22233. 가희와 키워드/가희와 키워드.java
@@ -1,12 +1,11 @@
-import java.io.BufferedReader;
-import java.io.IOException;
-import java.io.InputStreamReader;
+import java.io.*;
 import java.util.HashMap;
 import java.util.StringTokenizer;
 
 public class Main {
     public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
         StringTokenizer st = new StringTokenizer(br.readLine());
         HashMap<String, Integer> map = new HashMap<>();
         int N = Integer.parseInt(st.nextToken());
@@ -19,7 +18,9 @@ public class Main {
             for (int j = 0; j < split.length; j++) {
                 map.remove(split[j]);
             }
-            System.out.println(map.size());
+            bw.write(String.valueOf(map.size()));
+            bw.write("\n");
         }
+        bw.close();
     }
 }
