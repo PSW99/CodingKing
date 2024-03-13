5bddc48045ed72345c26173a428181bc5accc3bd
PSW99 <105043010+PSW99@users.noreply.github.com>
2024-03-13 21:38:48 +0900

    [Silver II] Title: DFS와 BFS, Time: 256 ms, Memory: 20432 KB -BaekjoonHub

---
+++ 백준/Silver/1260. DFS와 BFS/DFS와 BFS.java
@@ -0,0 +1,81 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.sql.ClientInfoStatus;
+import java.util.*;
+
+public class Main {
+    private static int N,M, V;
+    private static boolean[] visited;
+    private static StringBuilder sb;
+    private static List<List<Integer>> graph = new ArrayList<>();
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringTokenizer st = new StringTokenizer(br.readLine());
+        sb = new StringBuilder();
+        N = Integer.parseInt(st.nextToken());
+        M = Integer.parseInt(st.nextToken());
+        V = Integer.parseInt(st.nextToken());
+
+        for (int i = 0; i < N + 1; i++) {
+            graph.add(new ArrayList<>());
+        }
+        for (int i = 0; i < M; i++) {
+            st = new StringTokenizer(br.readLine());
+            int v = Integer.parseInt(st.nextToken());
+            int u = Integer.parseInt(st.nextToken());
+            graph.get(v).add(u);
+            graph.get(u).add(v);
+        }
+
+        visited = new boolean[N + 1];
+        visited[V] = true;
+
+        dfs(V, 0);
+        System.out.println(sb);
+        bfs(V);
+    }
+
+    private static void bfs(int start) {
+        boolean[] booleans = new boolean[N + 1];
+        Queue<Integer> queue = new LinkedList<>();
+        queue.add(start);
+        booleans[start] = true;
+        StringBuilder sb = new StringBuilder();
+
+        while (!queue.isEmpty()) {
+            int curV = queue.poll();
+            sb.append(curV).append(" ");
+
+            List<Integer> list = graph.get(curV);
+            list.sort(Comparator.naturalOrder());
+            for (int i : list) {
+                if (!booleans[i]) {
+                    queue.add(i);
+                    booleans[i] = true;
+                }
+            }
+        }
+
+        System.out.println(sb);
+    }
+
+    private static void dfs(int v,int count) {
+        sb.append(v).append(" ");
+        if (count == M) {
+            return;
+        }
+        List<Integer> list = graph.get(v);
+        list.sort(Comparator.naturalOrder());
+
+        for (int u : list) {
+            if (!visited[u]) {
+                visited[u] = true;
+                dfs(u, count + 1);
+            }
+        }
+
+    }
+
+
+}
