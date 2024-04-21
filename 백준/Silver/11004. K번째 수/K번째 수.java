442a1f1d2f375a6f32ae75b5d2e09ac7b267d493
PSW99 <105043010+PSW99@users.noreply.github.com>
2024-04-22 00:52:07 +0900

    [Silver V] Title: K번째 수, Time: 3580 ms, Memory: 460540 KB -BaekjoonHub

---
+++ 백준/Silver/11004. K번째 수/K번째 수.java
@@ -0,0 +1,25 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.ArrayList;
+import java.util.Comparator;
+import java.util.List;
+import java.util.StringTokenizer;
+
+public class Main {
+    private static int N, K;
+    private static List<Integer> list = new ArrayList<>();
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringTokenizer st = new StringTokenizer(br.readLine());
+        N = Integer.parseInt(st.nextToken());
+        K = Integer.parseInt(st.nextToken());
+        st = new StringTokenizer(br.readLine());
+        for (int i = 0; i < N; i++) list.add(Integer.valueOf(st.nextToken()));
+
+        list.sort(Comparator.naturalOrder());
+
+        System.out.println(list.get(K - 1));
+
+    }
+}
