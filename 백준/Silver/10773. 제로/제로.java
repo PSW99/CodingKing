83a4beec9ffe6371108375025c4952e2746fbaaa
PSW99 <105043010+PSW99@users.noreply.github.com>
2024-05-29 20:31:43 +0900

    [Silver IV] Title: 제로, Time: 248 ms, Memory: 23552 KB -BaekjoonHub

---
+++ 백준/Silver/10773. 제로/제로.java
@@ -0,0 +1,20 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.Stack;
+
+public class Main {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        int N = Integer.parseInt(br.readLine());
+        Stack<Integer> stack = new Stack<>();
+        for (int i = 0; i < N; i++) {
+            int n = Integer.parseInt(br.readLine());
+            if (n == 0) stack.pop();
+            else stack.push(n);
+        }
+        int answer = 0;
+        while (!stack.isEmpty()) answer += stack.pop();
+        System.out.println(answer);
+    }
+}
