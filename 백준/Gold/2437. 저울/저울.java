b6091edf7456679c0991634cc920e4fec4276e32
PSW99 <105043010+PSW99@users.noreply.github.com>
2024-07-03 22:17:07 +0900

    [Gold II] Title: 저울, Time: 132 ms, Memory: 14380 KB -BaekjoonHub

---
+++ 백준/Gold/2437. 저울/저울.java
@@ -0,0 +1,26 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.Arrays;
+import java.util.StringTokenizer;
+
+public class Main {
+    private static int N;
+    private static int[] arr;
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        N = Integer.parseInt(br.readLine());
+        arr = new int[N];
+        StringTokenizer st = new StringTokenizer(br.readLine());
+        for (int i = 0; i < N; i++) arr[i] = Integer.parseInt(st.nextToken());
+        
+        Arrays.sort(arr);
+        int num = 1;
+        for (int weight : arr) {
+            if (weight > num) break;
+            num += weight;
+        }
+
+        System.out.println(num);
+    }
+}
