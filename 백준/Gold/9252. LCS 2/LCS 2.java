8bde5d106411d16a90e64d1e563b5d171db7557b
PSW99 <105043010+PSW99@users.noreply.github.com>
2024-07-14 08:47:19 +0900

    [Gold IV] Title: LCS 2, Time: 144 ms, Memory: 18812 KB -BaekjoonHub

---
+++ 백준/Gold/9252. LCS 2/LCS 2.java
@@ -0,0 +1,44 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+
+public class Main {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+
+        String str1 = br.readLine();
+        String str2 = br.readLine();
+
+        int len1 = str1.length();
+        int len2 = str2.length();
+
+        int[][] dp = new int[len1 + 1][len2 + 1];
+        
+        for (int i = 1; i <= len1; i++) {
+            for (int j = 1; j <= len2; j++) {
+                if (str1.charAt(i - 1) == str2.charAt(j - 1)) dp[i][j] = dp[i - 1][j - 1] + 1;
+                else dp[i][j] = Math.max(dp[i - 1][j], dp[i][j - 1]);
+                
+            }
+        }
+
+        int lcsLength = dp[len1][len2];
+        System.out.println(lcsLength);
+
+        StringBuilder lcs = new StringBuilder();
+
+        int i = len1, j = len2;
+        while (i > 0 && j > 0) {
+            if (str1.charAt(i - 1) == str2.charAt(j - 1)) {
+                lcs.append(str1.charAt(i - 1));
+                i--; j--;
+            } else {
+                if (dp[i - 1][j] > dp[i][j - 1]) i--;
+                else j--;
+            }
+        }
+
+        if (lcsLength > 0) System.out.println(lcs.reverse());
+
+    }
+}
