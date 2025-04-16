40eb1c532690b4576fca1334222c26cbeb6ab6e5
PSW99 <steamdayz@naver.com>
2025-04-16 19:18:57 +0900

    [Gold IV] Title: 준규와 사과, Time: 108 ms, Memory: 14908 KB -BaekjoonHub

---
+++ 백준/Gold/5913. 준규와 사과/준규와 사과.java
@@ -0,0 +1,51 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.StringTokenizer;
+
+public class Main {
+    private static int K;
+    private static int[][] map = new int[5][5];
+    private static boolean[][] visited = new boolean[5][5];
+    private static int[] dx = {0, 1, 0, -1}, dy = {-1, 0, 1, 0};
+    private static int answer = 0;
+
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        K = Integer.parseInt(br.readLine());
+
+        for (int i = 0; i < K; i++) {
+            StringTokenizer st = new StringTokenizer(br.readLine());
+            int x = Integer.parseInt(st.nextToken()) - 1;
+            int y = Integer.parseInt(st.nextToken()) - 1;
+            map[x][y] = 1;
+        }
+
+        visited[0][0] = true;
+
+        backTracking(0, 0, 1);
+
+        System.out.println(answer);
+    }
+
+    private static void backTracking(int x, int y, int count) {
+        if (x == 4 && y == 4) {
+            if (count == 25 - K) {
+                answer++;
+            }
+        }
+
+        for (int i = 0; i < 4; i++) {
+            int nx = x + dx[i];
+            int ny = y + dy[i];
+
+            if (nx < 0 || nx >= 5 || ny < 0 || ny >= 5 || visited[nx][ny] || map[nx][ny] == 1) {
+                continue;
+            }
+
+            visited[nx][ny] = true;
+            backTracking(nx, ny, count + 1);
+            visited[nx][ny] = false;
+        }
+    }
+}
